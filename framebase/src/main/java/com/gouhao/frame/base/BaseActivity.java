f5b040a75351a3cd9f2820e9cb5e1f23f2eb98e8
gouhao <jakelyhao>
2017-02-07 14:26:33 +0800

    修改屏幕底部被系统底部操作栏挡住

--- framebase/src/main/java/com/gouhao/frame/base/BaseActivity.java
+++ framebase/src/main/java/com/gouhao/frame/base/BaseActivity.java
@@ -56,7 +56,7 @@ public abstract class BaseActivity<V extends ViewDataBinding, M extends Activity
             Window window = getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 
             window.setStatusBarColor(Color.TRANSPARENT);
