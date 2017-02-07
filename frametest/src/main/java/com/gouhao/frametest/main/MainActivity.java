75eabeb598382f5e9bbc7652f9afb9eb54f39557
gouhao <jakelyhao>
2017-02-07 15:15:20 +0800

    修改RecyclerView的UI

--- frametest/src/main/java/com/gouhao/frametest/main/MainActivity.java
+++ frametest/src/main/java/com/gouhao/frametest/main/MainActivity.java
@@ -1,11 +1,14 @@
 package com.gouhao.frametest.main;
 
 import android.databinding.DataBindingUtil;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
+import android.widget.Toast;
 
 import com.gouhao.frame.base.BaseActivity;
 import com.gouhao.frametest.R;
 import com.gouhao.frametest.databinding.ActivityMainBinding;
+import com.gouhao.frametest.databinding.ItemListBinding;
 
 /**
  * Created by gouhao on 2017/2/3 0003.
@@ -17,14 +20,24 @@ public class MainActivity extends BaseActivity<ActivityMainBinding,
     protected void initActivityDataBinding() {
         activityDataBinding = DataBindingUtil.inflate(getLayoutInflater(),
                 R.layout.activity_main, null, false);
-        activityDataBinding.recyclerView.setAdapter(new MainListAdapter());
-        activityDataBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        MainListAdapter adapter = new MainListAdapter();
+        adapter.setOnItemClickListener(new MainListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(ItemListBinding dataBinding, int position) {
+                Toast.makeText(MainActivity.this, "Item " + (position + 1) + " clicked", 2000).show();
+            }
+        });
+        activityDataBinding.recyclerView.setAdapter(adapter);
+        activityDataBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false));
+        activityDataBinding.recyclerView.setHasFixedSize(true);
+        activityDataBinding.recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
     }
 
     @Override
     protected void initActivityData() {
         activityData = new MainActivityData();
-        activityData.buttonText1.set("Button1");
+        activityData.buttonText1.set("Add Item");
         activityData.buttonText2.set("Show/Hide Label");
         activityData.isShowLabelText.set(true);
         activityData.labelText.set("This is a label!");
--- frametest/src/main/java/com/gouhao/frametest/main/MainListAdapter.java
+++ frametest/src/main/java/com/gouhao/frametest/main/MainListAdapter.java
@@ -1,9 +1,9 @@
 package com.gouhao.frametest.main;
 
-import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.gouhao.frametest.R;
@@ -13,22 +13,31 @@ import com.gouhao.frametest.databinding.ItemListBinding;
  * Created by gouhao on 2017/2/7 0007.
  */
 public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.Holder> {
+    private OnItemClickListener onItemClickListener;
     @Override
     public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-        ItemListBinding itemListBinding = DataBindingUtil.inflate(
-                LayoutInflater.from(parent.getContext()), R.layout.item_list, null, false);
+        final ItemListBinding itemListBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()), R.layout.item_list, parent, false);
         Holder holder = new Holder(itemListBinding);
         return holder;
     }
 
     @Override
-    public void onBindViewHolder(Holder holder, int position) {
+    public void onBindViewHolder(final Holder holder, final int position) {
         holder.getDataBinding().setText("Item " + (position + 1));
+        holder.getDataBinding().getRoot().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(onItemClickListener != null) {
+                    onItemClickListener.onItemClick(holder.getDataBinding(), position);
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 20;
+        return 5;
     }
 
     public class Holder extends RecyclerView.ViewHolder {
@@ -42,5 +51,13 @@ public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.Holder
         public ItemListBinding getDataBinding() {
             return dataBinding;
         }
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(ItemListBinding dataBinding, int position);
     }
 }
